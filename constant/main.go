4d40e7d2ab3ad5b949c7e525096cf3d9fc41e5da
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-07 18:21:05 +0530

    Added Pi and language values

--- constant/main.go
+++ constant/main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+const (
+	pi       = 3.14
+	language = "Go"
+)
+
 const p string = "death and taxes"
 
 func main() {
 	const q = 42
 	fmt.Println(" p - ", p)
 	fmt.Println(" q - ", q)
+	fmt.Println(" language - ", language)
+	fmt.Println(" pi - ", pi)
 }
