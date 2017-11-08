b95ac70432a6f88dfc6f0e3fe18b71158aff95fe
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-08 19:39:50 +0530

    changed function zero to zero1

--- usingpointers/nopointer/mainpnt.go
+++ usingpointers/nopointer/mainpnt.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func zero(x int) {
+func zero1(x int) {
 	fmt.Println("%p\n", &x)
 	fmt.Println(&x)
 	x = 0
@@ -12,6 +12,6 @@ func main() {
 	x := 5
 	fmt.Println("%p\n", &x)
 	fmt.Println(&x)
-	zero(x)
+	zero1(x)
 	fmt.Println(x)
 }
