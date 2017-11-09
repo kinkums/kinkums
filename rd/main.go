91c566d1e8383227daf5b474d2a72f4bc686d1a8
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-09 13:41:00 +0530

    Added details for exit

--- rd/main.go
+++ rd/main.go
@@ -6,30 +6,39 @@ import (
 )
 
 func main() {
+	var myOption string
 	c1 := make(chan string)
 	c2 := make(chan string)
 	go func() {
 		for {
-			c1 <- "eligible for a 5 minute break"
 			time.Sleep(time.Minute * 2)
+			c1 <- "eligible for a 5 minute break"
 		}
 	}()
 	go func() {
 		for {
-			c2 <- "eligible for a 30 minute break"
 			time.Sleep(time.Minute * 5)
+			c2 <- "eligible for a 30 minute break"
 		}
 	}()
 	go func() {
 		for {
 			select {
+			case msg2 := <-c2:
+				fmt.Print("Do you want to continue (Press C) or exit (Press X)")
+				fmt.Scan(&myOption)
+				if myOption == "C" || myOption == "c" {
+					fmt.Println(msg2)
+				} else if myOption == "X" || myOption == "x" {
+					break
+				} else {
+					fmt.Println("Invalid Option: Press 'C' or 'X' to continue ")
+				}
 			case msg1 := <-c1:
 				fmt.Println(msg1)
-			case msg2 := <-c2:
-				fmt.Println(msg2)
 			}
 		}
 	}()
-	var input string
-	fmt.Scanln(&input)
+	//var input string
+	//fmt.Scanln(&input)
 }
