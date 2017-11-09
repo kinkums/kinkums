5af9bb3c9bcaab5f5aacdfb3a27667ff5e821a59
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-09 17:11:03 +0530

    added dump folder

---
+++ dump/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Print("Enter your choice (C/E) ")
+	a := checkResponse()
+	fmt.Println(" Value of A is ", a)
+}
+
+func checkResponse() bool {
+	var userInput string
+	myInp, err := fmt.Scan(&userInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("User Input value ", myInp)
+	//const cont string = "C"
+	//const exits string = "E"
+	if myInp == 67 {
+		fmt.Println("Came into if")
+		return true
+	} else if myInp == 69 {
+		fmt.Println("Came into else")
+		return false
+	} else {
+		fmt.Println("Valid user values are C or E")
+		return checkResponse()
+	}
+}
