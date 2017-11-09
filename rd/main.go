419358675fa9f2b2a5424266089a88b58d9ee0f4
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-09 19:11:19 +0530

    crazy

--- rd/main.go
+++ rd/main.go
@@ -35,7 +35,7 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 				}*/
-				fmt.Sscan(userInput)
+				fmt.Scan(&userInput)
 				fmt.Println("User Input value ", userInput)
 				//myOption := checkResponse()
 				//fmt.Println("Option selected is ", myOption)
