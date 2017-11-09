89fa96141abd0490081d4724d6d11ea25f01fd36
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-09 23:34:52 +0530

    test scanf 1

--- temp/main.go
+++ temp/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var str1 string
-	//fmt.Println("Enter your Name ")
-	fmt.Scanf("Enter your Name ", &str1)
-	fmt.Printf("My Name is ", str1)
+	fmt.Println("Enter your Name ")
+	fmt.Scanf("%s", &str1)
+	fmt.Printf("My Name is %s", str1)
 
 }
