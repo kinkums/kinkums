332f4e438e78c07378f036c1126ddd4a2d47931e
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-09 15:20:43 +0530

    Added checkResponse method

--- rd/main.go
+++ rd/main.go
@@ -1,24 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+import "strings"
+import "log"
+import "time"
 
 func main() {
-	var myOption bool
+
 	c1 := make(chan string)
 	c2 := make(chan string)
 	go func() {
 		for {
-			c1 <- "eligible for a 5 minute break"
 			time.Sleep(time.Minute * 2)
+			c1 <- "eligible for a 5 minute break"
+
 		}
 	}()
 	go func() {
 		for {
-			c2 <- "eligible for a 30 minute break"
 			time.Sleep(time.Minute * 5)
+			c2 <- "eligible for a 30 minute break"
+
 		}
 	}()
 	go func() {
@@ -26,8 +28,8 @@ func main() {
 			select {
 
 			case msg2 := <-c2:
-				fmt.Print("Do you want to continue (Y/N)")
-				fmt.Scan(&myOption)
+				fmt.Print("Do you want to continue / exit (Continue/Exit)")
+				myOption := checkResponse()
 				if myOption == true {
 					fmt.Println(msg2)
 				} else {
@@ -40,4 +42,22 @@ func main() {
 	}()
 	var input string
 	fmt.Scanln(&input)
+}
+
+func checkResponse() bool {
+	var userInput string
+	_, errorOccured := fmt.Scanln(&userInput)
+	if errorOccured != nil {
+		log.Fatal(errorOccured)
+	}
+	const cont string = "Cont"
+	const exits string = "Exit"
+	if strings.EqualFold(cont, userInput) {
+		return true
+	} else if strings.EqualFold(exits, userInput) {
+		return false
+	} else {
+		fmt.Println("Valid user values are Cont or Exit")
+		return checkResponse()
+	}
 }
