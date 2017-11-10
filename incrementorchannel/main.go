b4c262d2440f7fcf518bc0d8c4bdf1bc848ff0a9
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-10 14:52:24 +0530

    Incrementor using Channels

--- incrementorchannel/main.go
+++ incrementorchannel/main.go
@@ -3,26 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	c := incrementor()
-	cSum := puller(c)
-	for n := range cSum {
-		fmt.Println(n)
-	}
+	c1 := incrementor("Foo:")
+	c2 := incrementor("Bar:")
+	c3 := puller(c1)
+	c4 := puller(c2)
+	fmt.Println("Final Counter: ", <-c3+<-c4)
 }
 
-// <-chan int, can only receive ints from the channel
-func incrementor() <-chan int {
+func incrementor(s string) chan int {
 	out := make(chan int)
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 20; i++ {
 			out <- i
+			fmt.Println(s, i)
 		}
 		close(out)
 	}()
 	return out
 }
 
-func puller(c <-chan int) <-chan int {
+func puller(c chan int) chan int {
 	out := make(chan int)
 	go func() {
 		var sum int
