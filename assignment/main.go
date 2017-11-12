9c49ea718bbb96fa5b85d16256c1b910b8a855f6
kinkums <32684400+kinkums@users.noreply.github.com>
2017-11-12 22:21:10 +0530

    test 1

--- assignment/main.go
+++ assignment/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -18,11 +19,13 @@ func clock() {
 	for {
 		time.Sleep(10 * time.Second)         //For Testing: change the sleep time to (1 * time.Seconds)
 		end := int(time.Since(st).Seconds()) //For Testing: change this value to time.Since(st).Seconds()
+
 		go addBreak(chan1)
-		if (end % 60) == 0 { //For testing: changing this value toa smaller number
+		if (end % 15) == 0 { //For testing: changing this value to a smaller number
 			chan1 <- 5
-			if (end % 180) == 0 {
+			if (end % 25) == 0 {
 				chan1 <- 30
+				go removeBreak(chan1)
 			}
 
 		}
@@ -32,6 +35,19 @@ func addBreak(chan1 chan int) {
 	br := <-chan1
 	breakTime += br //this variable will be increased by 5 mins every hour and after 3 hours, it will be increased by 30 mins
 	fmt.Println("break time ", breakTime)
+}
+
+func removeBreak(chan1 chan int) {
+	brTime := <-chan1
+	var option string
+	if brTime < timeCheck {
+		fmt.Println("Do you want to quit (y/n)?")
+		fmt.Scanf("%s", option)
+		if option == "Y" {
+			os.Exit(1)
+		}
+
+	}
 }
 
 /* You can write some methods for :
