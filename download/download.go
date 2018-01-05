15a061fde96a17630388a2ad1a813072ab826a52
kinkums <32684400+kinkums@users.noreply.github.com>
2018-01-05 22:54:19 +0530

    Test1

--- download/download.go
+++ download/download.go
@@ -12,17 +12,17 @@ func main() {
 	url := "https://github.com/kinkums/kinkums/tree/master/webupload/test/Picture002.jpg"
 	response, er := http.Get(url)
 	if er != nil {
-		log.Fatal("%s", er)
+		log.Fatal(er)
 	}
 	defer response.Body.Close()
-	file, err := os.Create("/images/pic2.jpg")
+	file, err := os.Create("./images/pic2.jpg")
 	if err != nil {
-		log.Fatal("%s", err)
+		log.Fatal(err)
 	}
 
 	_, err1 := io.Copy(file, response.Body)
 	if err1 != nil {
-		log.Fatal("%s", err1)
+		log.Fatal(err1)
 	}
 	file.Close()
 	fmt.Println("Image downloaded")
