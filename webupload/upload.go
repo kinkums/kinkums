d3b3eb50d3348ec3d55b871fd6f3b86e338ab9cc
kinkums <32684400+kinkums@users.noreply.github.com>
2018-01-10 17:19:51 +0530

    Added Queue

--- webupload/upload.go
+++ webupload/upload.go
@@ -4,9 +4,54 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/streadway/amqp"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendMsgToMQ() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", //name
+		false,   //durable
+		false,   //delete when unused
+		false,   //exclusive
+		false,   //no wait
+		nil,     //arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	body := "hello"
+	err = ch.Publish(
+		"",     //exchange
+		q.Name, //routing key
+		false,  //mandatory
+		false,  //immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+
+}
 
 func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -37,4 +82,5 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/upload", upload)
 	http.ListenAndServe(":9090", nil)
+	sendMsgToMQ()
 }
