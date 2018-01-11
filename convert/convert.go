61ca350878a951a0bb13cccde671c9cc35203b05
kinkums <32684400+kinkums@users.noreply.github.com>
2018-01-11 22:17:09 +0530

    Reading Image

---
+++ convert/convert.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+)
+
+func main() {
+	// Read image from file that already exists
+	existingImageFile, err := os.Open(".images\\5.jpg")
+	if err != nil {
+		// Handle error
+	}
+	defer existingImageFile.Close()
+
+	// Calling the generic image.Decode() will tell give us the data
+	// and type of image it is as a string. We expect "png"
+	imageData, imageType, err := image.Decode(existingImageFile)
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(imageData)
+	fmt.Println(imageType)
+}
