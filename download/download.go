5e416df58690da7e745add9b793c8494480c0d41
kinkums <32684400+kinkums@users.noreply.github.com>
2018-01-12 20:07:31 +0530

    Converter

--- download/download.go
+++ download/download.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/jpeg"
-	"image/png"
 	"io"
 	"log"
 	"net/http"
@@ -30,43 +27,4 @@ func main() {
 	file.Close()
 	fmt.Println("Image downloaded")
 
-	myImage := "sanju.jpg"
-	fmt.Println("The image is:", myImage)
-	imageToBeRead := readTheImage(myImage)
-	fmt.Println("Reading image data ...")
-	fmt.Println("Converting  image to png  ...")
-	convertToPNG(imageToBeRead)
-
-}
-
-func readTheImage(ImageFile string) (image image.Image) {
-	// open "test.jpg"
-	imgFile, err := os.Open(ImageFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// decode jpeg into image.Image
-	img, err := jpeg.Decode(imgFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	imgFile.Close()
-
-	return img
-}
-
-func convertToPNG(img image.Image) {
-	out, err := os.Create("Picture 400.png")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = png.Encode(out, img)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(img, "\n success... \n ")
 }
