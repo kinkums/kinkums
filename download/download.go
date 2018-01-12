58a6344d49d2cd3fe22a805adffd31abf012014e
kinkums <32684400+kinkums@users.noreply.github.com>
2018-01-12 19:35:44 +0530

    Converter

--- download/download.go
+++ download/download.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"log"
 	"net/http"
@@ -9,13 +12,13 @@ import (
 )
 
 func main() {
-	url := "https://github.com/kinkums/kinkums/tree/master/webupload/imageOutput/5.jpg"
+	url := "https://github.com/kinkums/kinkums/tree/master/webupload/imageOutput/rad.jpg"
 	response, er := http.Get(url)
 	if er != nil {
 		log.Fatal(er)
 	}
 	defer response.Body.Close()
-	file, err := os.Create("./images/5.jpg")
+	file, err := os.Create("rad.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,4 +30,43 @@ func main() {
 	file.Close()
 	fmt.Println("Image downloaded")
 
+	myImage := "rad.jpg"
+	fmt.Println("The image is:", myImage)
+	imageToBeRead := readTheImage(myImage)
+	fmt.Println("Reading image data ...")
+	fmt.Println("Converting  image to png  ...")
+	convertToPNG(imageToBeRead)
+
+}
+
+func readTheImage(ImageFile string) (image image.Image) {
+	// open "test.jpg"
+	imgFile, err := os.Open(ImageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode jpeg into image.Image
+	img, err := jpeg.Decode(imgFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	imgFile.Close()
+
+	return img
+}
+
+func convertToPNG(img image.Image) {
+	out, err := os.Create("Picture 400.png")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = png.Encode(out, img)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(img, "\n success... \n ")
 }
